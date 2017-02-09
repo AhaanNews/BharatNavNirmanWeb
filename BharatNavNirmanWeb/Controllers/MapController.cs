9929c262513ea7cd44de1175ef8b270432342796
AhaanNews <ahaannews@gmail.com>
2017-02-10 01:37:40 +0530

    Changes made by Rahul Kumar.

---
+++ BharatNavNirmanWeb/Controllers/MapController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BharatNavNirmanWeb.Controllers
+{
+    public class MapController : Controller
+    {
+        public ActionResult भारत_का_मानचित्र()
+        {
+            return View();
+        }
+    }
+}
---
+++ BharatNavNirmanWeb/Controllers/QuestionAnswerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BharatNavNirmanWeb.Controllers
+{
+    public class QuestionAnswerController : Controller
+    {
+        // GET: QuestionAnswer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BharatDarshan()
+        {
+            return View();
+        }
+    }
+}
